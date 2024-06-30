c2d7a0080ea23404cce207a52d270047e67a40fd
Sle2Pen <flatmind13@mail.ru>
2024-06-30 14:53:57 +0300

    initial commit

---
+++ src/game.c
@@ -0,0 +1,31 @@
+#include "game.h"
+
+int game_loop()
+{
+  return 0;
+}
+
+void* init_buffer()
+{
+  return NULL;
+}
+
+int release_buffer()
+{
+  return 0;
+}
+
+void swap_display_buffer()
+{
+
+}
+
+void render_display_buffer()
+{
+
+}
+
+void calculate_rendering_state()
+{
+
+}
---
+++ src/game_of_life.c
@@ -0,0 +1,6 @@
+#include "game.h"
+
+int main(int argc, const char* argv[]){
+	printf("Hello, World!");
+	return 0;
+}
